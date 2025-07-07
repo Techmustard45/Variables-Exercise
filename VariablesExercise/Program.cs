579322a00f867b55af23a95a2c106158bb7daae2
Elias <elias.lavrich@gmail.com>
2025-07-07 10:25:35 -0400

    added variables and printed them to console

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Elias";
+            int age = 21;
+            char inital = 'E';
+            bool isTrue = false;
+            double randNum = 12.65;
+            decimal randNum2 = 23.345m;
+
+            Console.WriteLine("Hello! My name is " + name + " and I'm " + age + " years old. My name begins with an " + inital + ". The statement \"Could I think of anything else to write\" is " + isTrue + ". Here are some numbers: " + randNum + " and " + randNum2 + ".");
         }
     }
 }
