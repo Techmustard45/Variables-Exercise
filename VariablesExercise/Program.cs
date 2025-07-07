cb4b70c41dc944f78dca2d82c8cb9c3eaa1af4c0
Elias <elias.lavrich@gmail.com>
2025-07-07 10:53:26 -0400

    used interpolation instead of concatenation

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -11,7 +11,8 @@
             double randNum = 12.65;
             decimal randNum2 = 23.345m;
 
-            Console.WriteLine("Hello! My name is " + name + " and I'm " + age + " years old. My name begins with an " + inital + ". The statement \"Could I think of anything else to write\" is " + isTrue + ". Here are some numbers: " + randNum + " and " + randNum2 + ".");
+            Console.WriteLine($"Hello! My name is {name} and I'm {age} years old. My name begins with an {inital}. The statement \"Could I think of anything else to write\" is {isTrue}. Here are some numbers: {randNum} and {randNum2}.");
+            ;
         }
     }
 }
